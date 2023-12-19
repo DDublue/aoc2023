bb94af4eb3dcb504d8fc8beb552ce4649ad501ed
DDublue <theonly1living@gmail.com>
2023-12-19 12:22:51 -0800

    day1 in c

--- day1/day1.c
+++ day1/day1.c
@@ -49,46 +49,51 @@ int char_to_int(char *c)
     return num;
 }
 
-int word_to_int(char *word)
+char word_to_char(char *word)
 {
-    int num;
+    char num;
     if (!strcmp(word, "one"))
     {
-        num = 1;
+        num = '1';
     }
     else if (!strcmp(word, "two"))
     {
-        num = 2;
+        num = '2';
     }
     else if (!strcmp(word, "three"))
     {
-        num = 3;
+        num = '3';
     }
     else if (!strcmp(word, "four"))
     {
-        num = 4;
+        num = '4';
     }
     else if (!strcmp(word, "five"))
     {
-        num = 5;
+        num = '5';
     }
     else if (!strcmp(word, "six"))
     {
-        num = 6;
+        num = '6';
     }
     else if (!strcmp(word, "seven"))
     {
-        num = 7;
+        num = '7';
     }
     else if (!strcmp(word, "eight"))
     {
-        num = 8;
+        num = '8';
     }
     else if (!strcmp(word, "nine"))
     {
-        num = 9;
+        num = '9';
     }
     return num;
+}
+
+int startswith(const char *pre, const char *str)
+{
+    return strncmp(pre, str, strlen(pre)) == 0;
 }
 
 int part1(FILE *in)
@@ -126,6 +131,8 @@ int part2(FILE *in)
 {
     int sum = 0;
     char *line = NULL;
+    char *nums[9] = {"one", "two", "three", "four", "five", "six", "seven",
+                     "eight", "nine"};
     size_t len = 0;
     ssize_t read;
 
@@ -143,7 +150,16 @@ int part2(FILE *in)
             }
             else
             {
-
+                char *substring = &line[i];
+                for (int j = 0; j < 9; j++)
+                {
+                    if (startswith(nums[j], substring))
+                    {
+                        digits[k] = word_to_char(nums[j]);
+                        k++;
+                        break;
+                    }
+                }
             }
         }
         if (k)
@@ -186,9 +202,6 @@ int main(int argc, char *argv[])
     (void)printf("Part 2: %d\n", p2);
 
     /* TESTING */
-    char *str = "one";
-    int number = word_to_int(str);
-    printf("%d\n", number);
 
     return 0;
 }
